7154214b5e13ea7bc685f007f432f6a3d5aabdbf
xuzhuojie <xuzhuojie@agree.com.cn>
2021-03-30 16:45:27 +0800

    对方法进行分类

--- 03_python_base/017_builtin_other.py
+++ 03_python_base/017_builtin_other.py
@@ -1,24 +1,15 @@
 print("=============内置函数=========")
 
-"""
-    
-"""
-
-# abs
-# 返回参数的绝对值
-print(abs(10))  # 10
-print(abs(-10))  # 10
-
-# all
-# 如果可迭代对象 所有元素 bool(x) 都是 True，返回 True。 如果可迭代对象为空，返回 True
-print(all([]))  # True
-print(all([1, 2, 0]))  # False
-
-# any
-# 对可迭代对象所有元素执行 bool(x) 如果一个为True ，返回True。如果可迭代对象为空，返回False
-print(any([]))  # False
-print(any(["", [], {}, ()]))  # False
-print(any(["", [], {}, (), 1]))  # True
+# 内置函数
+# 	进制转换 bin oct hex int
+# 	符号和序值转换 chr ord
+# 	数学运算类 divmod pow round abs
+# 	执行字符串 eval exec
+# 	判断类 all any
+# 	对象简单操作 hash len id
+# 	对象高级操作 zip max min sorted
+# 	多数值输出 dir locals vars
+# 	其他 help isinstnce  __import__
 
 # bin oct hex
 # bin
@@ -43,15 +34,6 @@ print(chr(97))  # a
 # ord
 print(ord("a"))  # 97
 
-# dir
-# 如果没有参数，返回作用域中的名称
-print(dir())
-# ['__annotations__', '__builtins__', '__cached__', '__doc__', '__file__', '__loader__', '__name__', '__package__', '__spec__']
-
-# 安装字母顺序返回对象的属性，如果对象提供了 __dir__ 则使用它，否则使用默认的 dir()
-print(dir(1))
-# ['__abs__', '__add__', ......,  'numerator', 'real', 'to_bytes']
-
 # divmod
 # 返回一个元组，元组为 (x//y, x%y)， 第一个元素是整数商，第二个是余数
 print(divmod(10, 3))  # (3, 1)
@@ -60,6 +42,28 @@ print(divmod(10, 3))  # (3, 1)
 print(pow(2, 3))  # x**y 结果：8
 print(pow(2, 3, 5))  # x**y % z 结果：3
 print("-----------------------")
+
+# round
+# 四舍五入
+print(round(3.4))  # 3
+print(round(3.5))  # 4
+
+# sum
+# 用于计算数字值类型的数字总和
+print(sum([1, 2, 3]))  # 6
+
+"""
+    sum 和 reduce
+    sum 用于计算数字值类型的数字总和
+    reduce 通过两个参数的函数，从左到右累加的应用于序列的项，将序列缩减为单个值。
+    reduce 可以通过设置函数实现更强大的功能
+"""
+
+# abs
+# 返回参数的绝对值
+print(abs(10))  # 10
+print(abs(-10))  # 10
+
 # eval
 #
 """
@@ -100,6 +104,8 @@ print(type(eval("(1,2)")))  # <class 'tuple'>
 
 # 更改不同域的变量值
 x = 10
+
+
 def test_eval():
     y = 20
     a = eval("x + y")
@@ -108,6 +114,8 @@ def test_eval():
     print("b = {}".format(b))
     c = eval("x + y", {"x": 2, "y": 3}, {"y": 4, "z": 5})  # globals locals 都指定
     print("c = {}".format(c))
+
+
 test_eval()
 print("x = {}".format(x))
 # 结果：
@@ -154,13 +162,16 @@ z = 30
 sum = x + y + z
 print(sum)
 """
+
+
 def test_exec():
-    y  = 20
+    y = 20
     exec(expr)  # 60
     exec(expr, {"x": 1, "y": 2})  # 33
     exec(expr, {"x": 1, "y": 2}, {"y": 3, "z": 4})  # 34
-test_exec()
 
+
+test_exec()
 
 """
 eval 和 exec 的区别
@@ -169,34 +180,30 @@ eval 和 exec 的区别
 
 """
 
+# all
+# 如果可迭代对象 所有元素 bool(x) 都是 True，返回 True。 如果可迭代对象为空，返回 True
+print(all([]))  # True
+print(all([1, 2, 0]))  # False
+
+# any
+# 对可迭代对象所有元素执行 bool(x) 如果一个为True ，返回True。如果可迭代对象为空，返回False
+print(any([]))  # False
+print(any(["", [], {}, ()]))  # False
+print(any(["", [], {}, (), 1]))  # True
+
 # hash
 # 在一次程序运行期间，相同的字符串（不可变对象）其hash值是一样的
 # 可hash 的数据类型即不可变数据类型，不可hash 的数据类型即可变数据类型
 print(hash("12345"))
 
-# help
-# 输出帮助信息
-# print(help(str))
+# len
+# 返回一个对象的长度
+print(len("abc"))
 
 # id
 # 返回一个对象的唯一标识，这保证了同时存在的对象是唯一的
 print(id(1))  # 140727915537040
 print(id(1) == id(1))  # True
-
-print("""""isinstance""")
-# isinstnce
-# 判断一个对象是否值给定数据类型的实例， 数据类型可以是一个元组
-print(isinstance("abc", str))  # True
-print(isinstance(1, (int, str)))    # True
-print(isinstance((1,), (int,str)))  # False
-
-# len
-# 返回一个对象的长度
-print(len("abc"))
-
-# locals
-# 返回一个包含当前域所有变量的字典
-print(locals())  # { '__name__': '__main__', '__doc__': None,'x': 10,  'i': 4, ......}
 
 # zip
 """
@@ -207,7 +214,7 @@ zip
 z = zip(["a", "b", "c"], [1, 2, 3])
 print(z)  # <zip object at 0x00000204207FFFC8>
 print(list(z))  # [('a', 1), ('b', 2), ('c', 3)]
-print(*zip("abc", [1,2,3], ("A","B","C")))  # ('a', 1, 'A') ('b', 2, 'B') ('c', 3, 'C')
+print(*zip("abc", [1, 2, 3], ("A", "B", "C")))  # ('a', 1, 'A') ('b', 2, 'B') ('c', 3, 'C')
 
 # max min
 # max
@@ -217,14 +224,14 @@ max
     默认数值型参数，取值大者；字符型参数，取字母表排序靠后者。
     参数key，其为一个函数，用来指定取最大值的方法。
     default命名参数用来指定最大值不存在时返回的默认值.
-    
+
     # 不同类型之间不能进行比较啊
 """
 # 简单求列表中的最大值
-print(max([1,23,4,99,10]))  # 99
+print(max([1, 23, 4, 99, 10]))  # 99
 
 # 求指定对象的最大值
-age_dic={'a1_age':18,'a2_age':20,'c1_age':100,'b3_age':300}
+age_dic = {'a1_age': 18, 'a2_age': 20, 'c1_age': 100, 'b3_age': 300}
 print(max(age_dic))  # c1_age
 
 # 设置求最大值方法
@@ -232,10 +239,10 @@ print(max(age_dic, key=lambda i: age_dic[i]))  # b3_age
 
 # min
 # 简单求列表中的最小值
-print(min([1,23,4,99,10]))  # 1
+print(min([1, 23, 4, 99, 10]))  # 1
 
 # 求指定对象的最小值
-age_dic={'a1_age':18,'a2_age':20,'c1_age':100,'b3_age':300}
+age_dic = {'a1_age': 18, 'a2_age': 20, 'c1_age': 100, 'b3_age': 300}
 print(min(age_dic))  # a1_age
 
 # 设置求最小值方法
@@ -245,11 +252,6 @@ print(min(age_dic, key=lambda i: age_dic[i]))  # a1_age
 # 把对象转换为字符串
 print(repr({"a": 1}))
 
-# round
-# 四舍五入
-print(round(3.4))  # 3
-print(round(3.5))  # 4
-
 # sorted
 """
 sorted
@@ -258,11 +260,11 @@ sorted
         key -- 主要是用来进行比较的元素，只有一个参数，具体的函数的参数就是取自于可迭代对象中，指定可迭代对象中的一个元素来进行排序。
         reverse -- 排序规则，reverse = True 降序 ， reverse = False 升序（默认）。
     返回值： 重新排序的列表
-    
+
 
 """
 # 没有设置key的排序
-print(sorted([2,3,5,6,1]))  # [1, 2, 3, 5, 6] 默认升序
+print(sorted([2, 3, 5, 6, 1]))  # [1, 2, 3, 5, 6] 默认升序
 
 #
 d = {1: 'D', 2: 'B', 3: 'B', 4: 'E', 5: 'A'}
@@ -281,15 +283,36 @@ print(l1)  # [1, 2, 3, 6] 改变原来的值
 print(sorted(l2))  # [1, 2, 3, 6] 没有改变原来的值，生成一个新的list
 print(l2)  # [1, 2, 6, 3]
 
-# sum
-# 用于计算数字值类型的数字总和
-print(sum([1,2,3]))  # 6
+# dir
+# 如果没有参数，返回作用域中的名称
+print(dir())
+# ['__annotations__', '__builtins__', '__cached__', '__doc__', '__file__', '__loader__', '__name__', '__package__', '__spec__']
+
+# 安装字母顺序返回对象的属性，如果对象提供了 __dir__ 则使用它，否则使用默认的 dir()
+print(dir(1))
+# ['__abs__', '__add__', ......,  'numerator', 'real', 'to_bytes']
+
+
+# locals
+# 返回一个包含当前域所有变量的字典
+print(locals())  # { '__name__': '__main__', '__doc__': None,'x': 10,  'i': 4, ......}
 
 # vars
 # 返回对象object的属性和属性值的字典对象，如果没有参数，就打印当前调用位置的属性和属性值 类似 locals()。
 test_vars = "test_vars"
 print(vars())  # {'__name__': '__main__', '__doc__': None,  ...... , 'test_vars': 'test_vars'}
 
+# help
+# 输出帮助信息
+# print(help(str))
+
+
+print("""""isinstance""")
+# isinstnce
+# 判断一个对象是否值给定数据类型的实例， 数据类型可以是一个元组
+print(isinstance("abc", str))  # True
+print(isinstance(1, (int, str)))  # True
+print(isinstance((1,), (int, str)))  # False
 
 # __import__
 """
